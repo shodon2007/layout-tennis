acc52a7c6eda0880126476e8dd5a71851b88fa50
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-10-07 17:40:15 +0700

    New Vegas

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,15 @@
 let isBurger = false;
 const burger = document.querySelector('.burger');
-const burger__body = document.querySelector('.burger__body')
-const body = document.querySelector('body');
-showBurger = () => {
-    body.style.overflow = "hidden";
-    burger.style.display = 'block';
-    burger__body.style.display = 'flex';
-    console.log("Hello world");
+
+
+const showBurger = () => {
+    burger.style.opacity = '1';
+    burger.style.zIndex = '2';
     return true;
 }
-hideBurger = () => {
-    body.style.overflow = "visible";
-    burger.style.display = 'none';
-    burger__body.style.display = 'none';
-    console.log("Привет мир");
+
+const hideBurger = () => {
+    burger.style.opacity = '0';
+    burger.style.zIndex = '-1';
     return false;
 }
