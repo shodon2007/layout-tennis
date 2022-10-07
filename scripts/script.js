d99f6f6a98309f3ebbdc6e77d218748aea60fb9e
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-10-07 21:53:24 +0700

    Hello world

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,19 @@
 let isBurger = false;
+let checkBurger = 0;
 const burger = document.querySelector('.burger');
 
 
 const showBurger = () => {
+    burger.style.display = 'block';
     burger.style.opacity = '1';
     burger.style.zIndex = '2';
     return true;
 }
 
 const hideBurger = () => {
+    setTimeout(() => {
+        burger.style.display = 0;
+    }, 200);
     burger.style.opacity = '0';
     burger.style.zIndex = '-1';
     return false;
