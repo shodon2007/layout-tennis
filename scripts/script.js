f9d0ce46b70d7255044a2b842488f553b9fc532e
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-10-10 18:58:06 +0700

    production version

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,26 @@
+// Slider
+let sliderItem = 0;
+sliderBody = document.querySelector('.trainers__body');
+sliderBodyLength = sliderBody.children.length;
+// Burger
 let isBurger = false;
 let checkBurger = 0;
 const burger = document.querySelector('.burger');
 
 
-const click = () => {
-    let plusBlock = document.querySelector('.plus__block');
-    let plus = document.querySelector('.plus');
-    plus.style.height = plusBlock.offsetWidth;
-    console.log(plus.offsetWidth);
+
+// Slider
+function slideClick(agr) {
+    agr == 1 ? sliderItem += 1 : sliderItem -= 1;
+    if (sliderItem > -1) {
+        sliderItem = 0;
+    } else if (sliderItem < -(sliderBodyLength - 6)) {
+        sliderItem = -(sliderBodyLength - 6);
+    }
+    sliderBody.style.left = `${sliderItem * 120}px`;
 }
 
+// Burger
 const showBurger = () => {
     burger.style.display = 'block';
     burger.style.opacity = '1';
