4f42d06f61e7d153eb2a429137c1554bba3e2652
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-12-08 15:17:55 +0700

    added animation on scrolling

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,43 @@
+const animItems = document.querySelectorAll('.anim-item');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    animOnScroll();
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset)) {
+                animItem.classList.add('active')
+            } else {
+                if (!animItem.classList.contains('no-delete')) {
+                    animItem.classList.remove('active');
+                }
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+}
+
+
+
+
+
 new Swiper('.swiper', {
     slidesPerView: 2,
 
@@ -21,6 +61,18 @@ new Swiper('.swiper', {
         prevEl: '.trainers__left',
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 let isBurger = false;
