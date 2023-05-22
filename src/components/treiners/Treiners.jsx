6e407a9d656e1aa5b6189371692c9244304627b2
shodon <about@shodon.ru>
2023-05-22 15:56:43 +0700

    added responsive treiners

--- src/components/treiners/Treiners.jsx
+++ src/components/treiners/Treiners.jsx
@@ -3,6 +3,9 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper';
 import SwipeLeft from '@/images/arrow-left.png';
 import SwipeRight from '@/images/arrow-right.png';
+
+import 'swiper/scss';
+
 import man1 from '@/images/man_1.png';
 import man2 from '@/images/man_2.png';
 import man3 from '@/images/man_3.png';
@@ -10,12 +13,62 @@ import man4 from '@/images/man_4.png';
 import man5 from '@/images/man_5.png';
 import man6 from '@/images/man_6.png';
 
-import 'swiper/scss';
-
 import styles from './Treiners.module.scss';
+import TreinerItem from './treinerItem/TreinerItem.jsx';
 
 const Treiners = () => {
-    console.log('.' + styles.buttonNext)
+    const treinerList = [
+        {
+            img: man1,
+            name: 'Nicola Men',
+            text: 'Arcu elit massa amet turpis vel consequat pellentesque sit.',
+        },
+        {
+            img: man2,
+            name: 'Petr Nicolaevich',
+            text: 'hello nice my name is very cool very good go bich',
+        },
+        {
+            img: man3,
+            name: 'Artur Bashkirow',
+            text: 'Behonge hover my mind of heel is very',
+        },
+        {
+            img: man4,
+            name: 'Maksim Gorkiy',
+            text: 'Absolutly going to school man month',
+        },
+        {
+            img: man5,
+            name: 'Nikita Krutanov',
+            text: 'I am a good coach with a lot of experience',
+        },
+        {
+            img: man6,
+            name: 'Valentina Supermenovna',
+            text: 'I was an athlete in the 1999 championship',
+        },
+        {
+            img: man1,
+            name: 'Sofia Asrorova',
+            text: 'No one will be left behind after me',
+        },
+        {
+            img: man2,
+            name: 'Sidjey Borisovna',
+            text: 'Never give up because everything is ahead',
+        },
+        {
+            img: man3,
+            name: 'Snova EtotChelov',
+            text: 'Without tennis, the world is empty for me.',
+        },
+        {
+            img: man4,
+            name: 'Micluderon Spawner',
+            text: 'If someone asks who is the best coach, he will answer that I am',
+        },
+    ];
     return (
         <div className={styles.main}>
             <div className={styles.top}>
@@ -29,147 +82,29 @@ const Treiners = () => {
                 <Swiper
                     modules={[Navigation]}
                     spaceBetween={10}
-                    slidesPerView={6}
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
+                    slidesPerView={2}
                     navigation={{
                         nextEl: '.' + styles.buttonNext,
                         prevEl: '.' + styles.buttonPrew,
                     }}
                     allowTouchMove={false}
+                    breakpoints={{
+                        400: {
+                            slidesPerView: 3,
+                        },
+                        660: {
+                            slidesPerView: 4,
+                        },
+                        1200: {
+                            slidesPerView: 6,
+                        }
+                    }}
                 >
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man1} alt="man1" />
-                        </div>
-                        <div className="man__name">
-                            Мерлин Манро
-                        </div>
-                        <div className="man__text">
-                            Arcu elit massa amet turpis vel consequat pellentesque sit.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man2} alt="man2" />
-                        </div>
-                        <div className="man__name">
-                            Претиум Адио
-                        </div>
-                        <div className="man__text">
-                            hello brothers, my name is pretioum adio, vel const to my teacher.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man3} alt="man3" />
-                        </div>
-                        <div className="man__name">
-                            Сэм Джонсон
-                        </div>
-                        <div className="man__text">
-                            I am the best man in the world. Questions?
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man4} alt="man4" />
-                        </div>
-                        <div className="man__name">
-                            Индиана Брозерс
-                        </div>
-                        <div className="man__text">
-                            Hi, i am Indiana Brothers.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man5} alt="man5" />
-                        </div>
-                        <div className="man__name">
-                            Лорем Лакус
-                        </div>
-                        <div className="man__text">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man6} alt="man6" />
-                        </div>
-                        <div className="man__name">
-                            Pulvinar aliquam
-                        </div>
-                        <div className="man__text">
-                            Arcu elit massa amet turpis vel consequat pellentesque sit.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man1} alt="man1" />
-                        </div>
-                        <div className="man__name">
-                            Pulvinar aliquam
-                        </div>
-                        <div className="man__text">
-                            Arcu elit massa amet turpis vel consequat pellentesque sit.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man2} alt="man2" />
-                        </div>
-                        <div className="man__name">
-                            Pulvinar aliquam
-                        </div>
-                        <div className="man__text">
-                            Arcu elit massa amet turpis vel consequat pellentesque sit.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man3} alt="man3" />
-                        </div>
-                        <div className="man__name">
-                            Pulvinar aliquam
-                        </div>
-                        <div className="man__text">
-                            Arcu elit massa amet turpis vel consequat pellentesque sit.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man4} alt="man4" />
-                        </div>
-                        <div className="man__name">
-                            Pulvinar aliquam
-                        </div>
-                        <div className="man__text">
-                            Arcu elit massa amet turpis vel consequat pellentesque sit.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man5} alt="man5" />
-                        </div>
-                        <div className="man__name">
-                            Pulvinar aliquam
-                        </div>
-                        <div className="man__text">
-                            Arcu elit massa amet turpis vel consequat pellentesque sit.
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="man__img">
-                            <img src={man6} alt="man6" />
-                        </div>
-                        <div className="man__name">
-                            Pulvinar aliquam
-                        </div>
-                        <div className="man__text">
-                            Arcu elit massa amet turpis vel consequat pellentesque sit.
-                        </div>
-                    </SwiperSlide>
+                    {treinerList.map(trainer => {
+                        return <SwiperSlide>
+                            <TreinerItem trainer={trainer} />
+                        </SwiperSlide>
+                    })}
                 </Swiper>
             </div>
         </div>
---
+++ src/components/treiners/treinerItem/TreinerItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './TreinerItem.module.scss';
+
+const TreinerItem = ({ trainer }) => {
+    return (
+        <div className={styles.main}>
+            <img src={trainer.img} alt="hello" />
+            <div className={styles.name}>{trainer.name}</div>
+            <div className={styles.text}>{trainer.text}</div>
+        </div>
+    )
+}
+
+export default TreinerItem
